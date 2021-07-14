13ed5a25340561da4907289ad45f8dafaac1fd36
Atru8729 <a.truskauskas1@gmail.com>
2021-07-14 20:48:12 +0300

    tobulinimas

--- Kitchen.js
+++ Kitchen.js
@@ -1,21 +1,41 @@
 class Kitchen {
     constructor(virejoName) {
-        this.produktuSarasas = [];
+        this.list = [];
         this.virejoName = virejoName;
     }
     intro() {
         console.log(`Sveiki atvyke i ${this.virejoName} virtuve!`);
     }
-    nupirkta(produktas, kiekis) {
-        let krepselis = { produktas, kiekis }
-        this.produktuSarasas.push(krepselis);
+    nupirkta(pavadinimas, kiekis) {
+        let krepselis = { pavadinimas, kiekis }
+        this.list.push(krepselis);
         console.log(krepselis);
     }
     turimiProduktai() {
-
+        for (let i = 0; i < this.list.length; i++) {
+            const list = this.list[i];
+            const naujasPavadinimas = this.isDidziosiosRaides(list.pavadinimas);
+            console.log(`${i + 1}) ${list.pavadinimas}: ${list.kiekis}`);
+        }
     }
-    sunaudota() {
+    isDidziosiosRaides(tekstas) {
+        let atsakymas = '';
+        //reikia rasti 1 raide
+        //rasti likusias raides
+        //1 rasta raide versti i didziaja
+        //didziaja raide sujungti su likusiomis raidemis
+        return atsakymas;
+    }
 
+    sunaudota(pavadinimas, sunaudotasKiekis) {
+        for (let i = 0; i < this.list.length; i++) {
+            const sunaudotasKiekis = this.list[i];
+            console.log(pavadinimas, sunaudotasKiekis);
+            // if (pavadinimas === ) {
+
+            // }
+
+        }
     }
     sugedesProduktas() {
 
