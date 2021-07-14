2541c119b8616c7a4a642f81fa8aa634fb2f02e1
Atru8729 <a.truskauskas1@gmail.com>
2021-07-14 19:28:50 +0300

    intro+nupirkta

--- Kitchen.js
+++ Kitchen.js
@@ -1,7 +1,26 @@
 class Kitchen {
-    constructor() {
+    constructor(virejoName) {
+        this.produktuSarasas = [];
+        this.virejoName = virejoName;
+    }
+    intro() {
+        console.log(`Sveiki atvyke i ${this.virejoName} virtuve!`);
+    }
+    nupirkta(produktas, kiekis) {
+        let krepselis = { produktas, kiekis }
+        this.produktuSarasas.push(krepselis);
+        console.log(krepselis);
+    }
+    turimiProduktai() {
 
     }
+    sunaudota() {
+
+    }
+    sugedesProduktas() {
+
+    }
+
 }
 
 module.exports = Kitchen;
